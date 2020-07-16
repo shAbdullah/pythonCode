3dcf685912ae239b5ecdf837b6ca9d1825c47672
Abdullah-Ibrahime <aiaas2010@gmail.com>
2020-07-16 16:34:02 +0300

    3rd commit

--- build/lib/test/test_calculator.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from example_pkg_kh.calculator import Calculator
-class TestCalculator(unittest.TestCase):
-    def test_returns_0_for_empty_arguments(self):
-        calc = Calculator()
-        self.assertEqual(0, calc.sum())
-    def test_returns_sum(self):
-        calc = Calculator()
-        self.assertEqual(3, calc.sum(1, 2))
