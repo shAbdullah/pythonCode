0970ee1668fc0fdccb02fd41e0e173ae3a83706c
Abdullah Al-shmsan <38299402+shAbdullah@users.noreply.github.com>
2020-07-16 16:17:53 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import setuptools
 with open("README.md", "r") as fh:
-long_description= fh.read()
+long_description = fh.read()
+
+
 setuptools.setup(
     name="example-pkg-kh", 
     version="0.0.1",
